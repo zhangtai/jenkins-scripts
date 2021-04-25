a033a4d4ae7eb4a8532612e60280d4514453a365
Tai Zhang <tai.zhang@outlook.com>
2021-04-25 10:43:17 +0800

    Add nested aux

--- jobs/create-auxiliry-template.groovy
+++ jobs/create-auxiliry-template.groovy
@@ -3,6 +3,8 @@ import com.cloudbees.hudson.plugins.modeling.Attribute
 import com.cloudbees.hudson.plugins.modeling.controls.*
 
 def jenkins = Jenkins.get()
+def aus = jenkins.allItems(AuxModel)
+def aAnsible = aus.find { it.id == "Aux Ansible" }
 
 def newTextAttr = new Attribute("who", "Who?", (new TextFieldControl()))
 newTextAttr.control.setDefaultValue("tiger")
@@ -15,8 +17,10 @@ def newChoiceAttr = new Attribute("what", "What?", new ChoiceControl(
   ChoiceControl.Mode.RADIO_BUTTON
 ))
 
+def newNestedAuxAttr = new Attribute("scan", "Scan Tool", new NestedAuxModelControl(NestedAuxModelControl.Mode.SINGLE, aAnsible))
+
 def newAuxModel = new AuxModel(jenkins, "Aux1")
-newAuxModel.setAttributes([newTextAttr, newChoiceAttr])
+newAuxModel.setAttributes([newTextAttr, newChoiceAttr, newNestedAuxAttr])
 newAuxModel.save()
 jenkins.save()
 jenkins.reload()
