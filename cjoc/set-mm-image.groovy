e0fc0c12165b2c0a269d41613a3dd61970f827d2
Tai Zhang <tai.zhang@outlook.com>
2022-03-01 06:37:09 +0000

    Update script

--- cjoc/set-mm-image.groovy
+++ cjoc/set-mm-image.groovy
@@ -2,7 +2,11 @@ import com.cloudbees.opscenter.server.model.ManagedMaster
 
 def jenkins  = Jenkins.get()
 jenkins.allItems(ManagedMaster).each { controller ->
-  controller.configuration.setImage('CloudBees CI - Managed Master - 2.319.2.5')
+  controller.configuration.setImage('CloudBees CI - Managed Master - 2.319.3.3')
+  controller.save()
 }
+
+jenkins.save()
+jenkins.reload()
 
 null
---
+++ plugins/durable-task.groovy
@@ -0,0 +1,2 @@
+// Enable debug mode
+org.jenkinsci.plugins.durabletask.BourneShellScript.LAUNCH_DIAGNOSTICS=true
