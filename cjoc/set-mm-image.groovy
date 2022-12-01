baa57537ad083b927b1f2db8bede497ef7e9ec1d
Tai Zhang <tai.zhang@outlook.com>
2022-12-01 09:24:13 +0800

    Update cjoc scripts

--- cjoc/set-mm-image.groovy
+++ cjoc/set-mm-image.groovy
@@ -1,8 +1,23 @@
 import com.cloudbees.opscenter.server.model.ManagedMaster
 
+// operations-center-server.jar plugin
+
 def jenkins  = Jenkins.get()
-jenkins.allItems(ManagedMaster).each { controller ->
-  controller.configuration.setImage('CloudBees CI - Managed Controller - 2.361.4.1')
-  // controller.configuration.yaml
-  controller.save()
+jenkins.allItems(ManagedMaster).each { c ->
+  
+  // Set controller image
+  c.configuration.setImage('CloudBees CI - Managed Controller - 2.361.4.1')
+  // .configuration.yaml
+  // .stopAction(false)
+  // .provisionAndStartAction()
+  // .persistedState
+  // .resource
+  // .resource.isStale()
+
+  // Stop all stale controllers
+  if (c.resource?.isStale() && c.state.name() == "APPROVED") c.stopAction(false)
+
+  // Start all stopped controllers
+  if (c.state.name() == "CREATED") println(c.provisionAndStartAction())
+  c.save()
 }
